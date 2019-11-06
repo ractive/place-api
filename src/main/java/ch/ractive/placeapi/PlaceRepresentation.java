18bad2dba12de193f2359a8ad16d8827cc37e9cc
james <james.bergamin@local.ch>
2019-11-06 09:18:06 +0100

    Add javadoc

--- src/main/java/ch/ractive/placeapi/PlaceRepresentation.java
+++ src/main/java/ch/ractive/placeapi/PlaceRepresentation.java
@@ -1,17 +1,28 @@
 package ch.ractive.placeapi;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Value;
-
 import java.time.LocalTime;
 import java.util.List;
+
+import lombok.Value;
 
 @Value
 public class PlaceRepresentation {
     private String name;
     private String address;
 
-    @Value
+	/**
+	 * Allows presenting opening hours in a form, where weekdays with the same
+	 * opening hours are "compacted", e.g. like:
+	 *
+	 *
+	 * <table>
+	 *     <tr><td>Montag</td><td>geschlossen</td></tr>
+	 *     <tr><td>Dienstag - Freitag</td><td>11:30 - 15:00<br/>18:30 - 00:00</td></tr>
+	 *     <tr><td>Samstag</td><td>18:00 - 00:00</td></tr>
+	 *     <tr><td>Sonntag</td><td>11:30 - 15:00</td></tr>
+	 * </table>
+	 */
+	@Value
     public static class OpeningHoursRange {
         private String weekdayStart;
         private String weekdayEnd;
